388f45fbb95936abc53001995294605e34de6f38
Piotr <greenerenator@gmail.com>
2020-03-21 12:01:46 +0100

    Register appointments module in gin router

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-clinic/appointments"
+)
 
 func main() {
 	// Creates a gin router with default middleware:
 	// logger and recovery (crash-free) middleware
 	router := gin.Default()
+
+	RegisterModules(router)
+
 	router.Run()
+}
+
+func RegisterModules(router gin.IRouter) {
+	v1 := router.Group("/api")
+
+	appointments.RegisterModule(v1.Group("/appointments"))
 }
