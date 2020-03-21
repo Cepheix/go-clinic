284fb44a879f415b6e8fce63d3c70230fbe6f55d
Piotr <greenerenator@gmail.com>
2020-03-21 14:06:49 +0100

    Register handler for creating new patient in gin router

--- appointments/module.go
+++ appointments/module.go
@@ -1,7 +1,10 @@
 package appointments
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-clinic/appointments/application"
+)
 
 func RegisterModule(routerGroup *gin.RouterGroup) {
-
+	routerGroup.POST("patients", application.CreatePatient)
 }
