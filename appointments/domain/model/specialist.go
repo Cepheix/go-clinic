7315d85fde572e3a2d8281bc72e4ce8663c0b48c
Piotr <greenerenator@gmail.com>
2020-04-04 12:26:23 +0200

    Add domain model method to add new ability

--- appointments/domain/model/specialist.go
+++ appointments/domain/model/specialist.go
@@ -4,4 +4,16 @@ type Specialist struct {
 	ID        int
 	FirstName string
 	LastName  string
+	Ability   []SpecialistAbility
+}
+
+func (specialist *Specialist) AddAbility(abilityType Procedure) {
+	ability := SpecialistAbility{AbilityType: abilityType}
+	specialist.Ability = append(specialist.Ability, ability)
+}
+
+type SpecialistAbility struct {
+	ID           int
+	AbilityType  Procedure
+	SpecialistID int
 }
